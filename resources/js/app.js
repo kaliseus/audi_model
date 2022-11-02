4d3a8bb4c6a851ee583d9a3d5118cdef35c2734e
Kaliseus <Kaliseus@protonmail.com>
2022-11-01 21:01:41 -0300

    Correção bootstrap.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/js/bootstrap';
 import {createApp} from 'vue'
 import App from './app.vue'
 
