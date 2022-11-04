9b08cae107055f24da253d8f9a3069ad8d8b4efd
Kaliseus <Kaliseus@protonmail.com>
2022-11-03 21:16:38 -0300

    criação das migrations

--- resources/pages/app.vue
+++ resources/pages/app.vue
@@ -3,6 +3,7 @@
     <Swiper></Swiper>
     <featured-vehicles></featured-vehicles>
     <Filter-home></Filter-home>
+    <Widgets></Widgets>
 
 </template>
 
@@ -11,10 +12,11 @@ import Navbar from "../js/components/Navbar.vue";
 import Swiper from "../js/components/Swiper.vue";
 import FilterHome from "../js/components/Filterhome.vue";
 import FeaturedVehicles from "../js/components/Featured-vehicles.vue";
+import Widgets from "../js/components/Widgets.vue";
 
 export default {
     name: "app",
-    components: {FeaturedVehicles, FilterHome, Swiper, Navbar},
+    components: {Widgets, FeaturedVehicles, FilterHome, Swiper, Navbar},
 }
 </script>
 
